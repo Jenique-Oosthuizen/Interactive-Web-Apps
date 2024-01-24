68a599a109f24e422099fa571203e93b4b103d84
Jenique-Oosthuizen <jeniqueoosthuizen@gmail.com>
2024-01-24 18:17:09 +0200

    IWA2

---
+++ IWA2/challenge3.js
@@ -0,0 +1,13 @@
+/*
+ It is important to show the following message in order to;
+  - assure users
+  - scare hackers
+  - impress investors
+*/
+var warnMessage = 'Security scan starting';
+console.warn(warnMessage);
+
+/* It is important to let user know when they can close the page  */
+
+var infoMessage = 'Please wait for scan to complete before closing the browser.';
+console.info(infoMessage);
