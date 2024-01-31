97ab8fec30146e41fd61922a6369364084fbe8b2
Jenique-Oosthuizen <jeniqueoosthuizen@gmail.com>
2024-01-31 18:13:01 +0200

    IWA4

---
+++ IWA4/challenge1.js
@@ -0,0 +1,35 @@
+const date = 2050
+const Status = 'student'
+let count = 0
+
+if (date == 2050) {
+	console.log("January", 'New Year’s Day')
+	console.log("March", 'Human Rights Day')
+	date = 'April'
+	console.log(date, 'Family Day')
+	console.log(date, 'Freedom Day')
+	count = count + 4
+
+	if (Status == "student") {
+	  console.log('June', 'Youth Day')
+		count = count + 1
+  }
+
+	console.log('August', 'Women’s Day')
+	console.log('September', 'Heritage Day')
+	date = 'December'
+	console.log(date, 'Day of Reconciliation')
+	count = count + 3
+
+	if (Status == "parent") {
+	  console.log(date, 'Christmas Day')
+		count = count + 1
+  }
+
+	console.log(date, 'Day of Goodwill')
+	count = count + 1
+}
+
+console.log('Your status is:', Status)
+console.log('The year is:', date)
+console.log('The total holidays is:', count)
